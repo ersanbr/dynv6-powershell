546bd57ddd3b72489d84830be12e87e789416873
ersanbr <ers2003@gmail.com>
2017-11-24 09:40:35 -0200

    Update dynv6.ps1
    Ajustado nos casos de o adaptador receber mais de um ipv6, no caso de usar o agendador de tarefas é necessário rodar com privilégios elevados.

--- dynv6/dynv6.ps1
+++ dynv6/dynv6.ps1
@@ -1,4 +1,15 @@
-$current = Get-NetIPAddress -AddressFamily IPv6| where { $_.PrefixOrigin -match 'RouterAdvertisement'}| where { $_.SuffixOrigin -match 'Link'} | findstr -I -N IPAddress | %{ $_.Split(' ')[10]; }
+# Verify number of ipv6 has listed
+$current_lines = Get-NetIPAddress -AddressFamily IPv6| where { $_.PrefixOrigin -match 'RouterAdvertisement'}| where { $_.SuffixOrigin -match 'Link'} | where {$_.AddressState -match 'Preferred'} | findstr -I -N IPAddress | %{ $_.Split(' ')[10]; } | Measure-Object | findstr.exe -I -N Count | %{ $_.split(' ')[5];}
+
+# if is different of the 1, restart the adapter
+if ($current_lines -ne 1){
+	 Get-NetAdapter | ? Name -eq Ethernet | Disable-NetAdapter -Confirm:$false
+	 Get-NetAdapter | ? Name -eq Ethernet | Enable-NetAdapter -Confirm:$false
+	 Start-Sleep -s 10
+}
+
+# Get actual ipv6
+$current =  Get-NetIPAddress -AddressFamily IPv6| where { $_.PrefixOrigin -match 'RouterAdvertisement'}| where { $_.SuffixOrigin -match 'Link'} | where {$_.AddressState -match 'Preferred'} | findstr -I -N IPAddress | %{ $_.Split(' ')[10]; }
 
 $file = ($HOME + "\dynv6.addr6")
 
@@ -8,6 +19,7 @@ $token = "YOUR TOKEN"
 
 $hostname ="YOUR_HOSTNAME.dynv6.net"
 
+# Test exist $file, if not create then
 if (Test-Path -Path $file) {
         $old = Get-Content $file
     } else {
@@ -15,6 +27,7 @@ if (Test-Path -Path $file) {
 		$old = Get-Content $file
 	}
 
+# Test old ipv6 with collect, case equals not update dynv6
 if ($old -eq $current) {
     ((Get-Date -format dd/MM/yyyy-HH:mm:s) + " - IPv6 address unchanged") >> $log
     } else {
